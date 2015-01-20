a6af78000e2ee34a7902b3d8fa626d283996924a
Akhil Kodali <akhil.kodali@gmail.com>
2015-01-20 16:59:41 -0500

    change image for shell command

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:latest /bin/bash
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t akhil/kafka-docker:0.8.1.1 /bin/bash
